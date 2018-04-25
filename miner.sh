08d271474a5ffef5aec3a26ade3b3632feeb8445
Mr.Anderson <31638554+infktr@users.noreply.github.com>
2018-04-25 13:00:12 -0700

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,10 +1,5 @@
 #This "CPU" miner is for ubuntu and linux systems only!!!!
-#Do a nano myminer.sh press enter
-#Feel free to copy the contents to your file by selecting all of the commands and (comments if you like)
-#For nano right click and paste or crtl-shift-v
-#To exit nano do a ctrl-x then press y and press enter
-# then run chmod +x myminer.sh
-#Run with ./myminer.sh
+
 
 
 
@@ -27,11 +22,3 @@ cd bin
 ./xmr-stak-cpu
 
 
-#Change dir to xmr-stak-cpu, then change directory to build-x86_64-linux-gnu, then change directory bin,
-#  cd xmr-stak-cpu
-#  cd build-x86_64-linux-gnu
-#  cd bin
-#  executing      ./xmr-stak-cpu     will give you the default config for the miner again
-#Copy and paste the default config the program output for you in the config.txt as it corresponds with the file.
-#Learning the specifications of your processor will help you with you limitations
-#enjoy!
